c5a28e2e821631e08b7f2065ee819637c72f903e
Stephan Brezinsky <stephan@Stephans-iMac.local>
2023-09-11 13:24:42 -0400

    First commit

---
+++ users/birchbeast/config.h
@@ -0,0 +1,15 @@
+/* Copyright 2022 @birchbeast
+ *
+ * Global config across keymaps
+ */
+#define TAPPING_FORCE_HOLD
+#undef TAPPING_TERM
+#define TAPPING_TERM 240
+#define TAPPING_TERM_PER_KEY
+#define RETRO_TAPPING
+#define RETRO_TAPPING_PER_KEY // Turn this on to fix holding cmd or space
+
+#undef MOUSEKEY_DELAY
+#define MOUSEKEY_DELAY 5
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL 17
